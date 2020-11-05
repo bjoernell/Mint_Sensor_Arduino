79edd42e121406ebbbc771a86224d20bf003f572
bjoernell <bjoern.ellwert@gmail.com>
2020-11-05 12:03:33 +0100

    added ultrasonic sensor routines. Need to be tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,66 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define ReUnTrig  13
+#define ReUnEch   12
+
+#define LiUnTrig  11
+#define LiUnEch   10
+
+#define ReObTrig  9
+#define ReObEch   8
+
+#define LiObTrig  6
+#define LiObEch   7
+
+#define Speaker   5
+
+String ReUnName = "ReUn";
+String LiUnName = "LiUn";
+String ReObName = "ReOb";
+String LiObName = "LiOb";
+
+long duration;
+int distance; 
+
+void USMessung(int UsSenTrigVal, int UsSenEchVal, String UsSenName) {
+  
+  digitalWrite(UsSenTrigVal, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(UsSenTrigVal, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(UsSenTrigVal, LOW);
+  
+  duration = pulseIn(UsSenEchVal, HIGH);
+  
+  distance = duration * 0.034 / 2;
+  
+  Serial.print(UsSenName + distance + "cm");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+void setup() {
+  pinMode(ReUnTrig, OUTPUT);
+  pinMode(ReUnEch, INPUT); 
+
+  pinMode(LiUnTrig, OUTPUT);
+  pinMode(LiUnEch, INPUT); 
+
+  pinMode(ReObTrig, OUTPUT);
+  pinMode(ReObEch, INPUT); 
+
+  pinMode(LiObTrig, OUTPUT);
+  pinMode(LiObEch, INPUT); 
+
+  pinMode(Speaker, OUTPUT);
+
+  Serial.begin(115200); 
+}
+
+void loop(){
+ USMessung(ReObTrig, ReUnEch, ReObName);
+ USMessung(LiObTrig, LiObEch, LiObName);
+ USMessung(ReUnTrig, ReUnEch, ReUnName);
+ USMessung(LiUnTrig, LiUnEch, LiUnName);
 }
