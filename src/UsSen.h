4d08f0ff7a112b4b2842af436afb5d3cc23a7977
bjoernell <bjoern.ellwert@gmail.com>
2020-11-10 13:50:20 +0100

    added timeout

--- src/UsSen.h
+++ src/UsSen.h
@@ -4,30 +4,41 @@ class UltrasonicSensor
 {
 private:
   int _trig_pin, _echo_pin;
-  String _name;  
+  String _name;
+
 public:
-  UltrasonicSensor(int trig_pin, int echo_pin, String name) : _trig_pin(trig_pin), _echo_pin(echo_pin), _name(name) {
+  UltrasonicSensor(int trig_pin, int echo_pin, String name) : _trig_pin(trig_pin), _echo_pin(echo_pin), _name(name)
+  {
     pinMode(_trig_pin, OUTPUT);
-    pinMode(_echo_pin, INPUT); 
+    pinMode(_echo_pin, INPUT);
   };
-  
-  void distance() {
+
+  void distance()
+  {
     unsigned long duration;
     double distance;
-    
+
     digitalWrite(_trig_pin, LOW);
     delayMicroseconds(2);
-    
+
     digitalWrite(_trig_pin, HIGH);
     delayMicroseconds(10);
-    
+
     digitalWrite(_trig_pin, LOW);
-    
-    duration = pulseIn(_echo_pin, HIGH);
-    
+
+    duration = pulseIn(_echo_pin, HIGH, 117); //117 Microsekunden = timeout zeit für 2m Entfernung
+
     distance = duration * 0.034 / 2;
     //return distance;
-    Serial.print(_name);
-    Serial.println(distance);
-  };
+    if (distance != 0)
+    {
+      Serial.print(_name);
+      Serial.println(distance);
+    }
+    if (distance = 0)
+    {
+      Serial.print(_name);
+      Serial.println("error");
+    }
+  }
 };
